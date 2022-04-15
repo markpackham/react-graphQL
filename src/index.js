f27444532a17136402d3af62f4da7b9bc4db38d5
mark <markpackham1@gmail.com>
2022-04-15 11:52:17 +0100

    Revise React and GraphQL https://youtu.be/gAbIQx26wSI?t=577

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// What we want to connect to https://rickandmortyapi.com/graphql
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
