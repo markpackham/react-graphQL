cc6b3299cf2cbeb29d7432cc55669f593ed98652
mark <markpackham1@gmail.com>
2022-04-15 20:46:09 +0100

    setup ApolloProvider
    https://youtu.be/gAbIQx26wSI?t=840

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: InMemoryCache(),
+});
 
 // What we want to connect to https://rickandmortyapi.com/graphql
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
