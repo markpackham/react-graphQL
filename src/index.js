cefde5a5eae16f48f2491648dd5aea8dd5e32d0e
mark <markpackham1@gmail.com>
2022-04-15 20:50:09 +0100

    add CharacterList page
    https://youtu.be/gAbIQx26wSI?t=1031

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "https://rickandmortyapi.com/graphql",
-  cache: InMemoryCache(),
+  cache: new InMemoryCache(),
 });
 
 // What we want to connect to https://rickandmortyapi.com/graphql
