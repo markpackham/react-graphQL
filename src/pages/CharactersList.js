6cc6819708c9e7cf2e21ebd69fd0da975178eb51
mark <markpackham1@gmail.com>
2022-04-18 13:44:53 +0100

    refactor CharacterList using reuseable hook
    https://youtu.be/gAbIQx26wSI?t=1912

--- src/pages/CharactersList.js
+++ src/pages/CharactersList.js
@@ -1,23 +1,9 @@
 import React from "react";
-import { useQuery, gql } from "@apollo/client";
+import { useCharacters } from "../hooks/useCharacters";
 import "./CharactersList.css";
 
-const GET_CHARACTERS = gql`
-  query {
-    characters {
-      results {
-        id
-        name
-        image
-      }
-    }
-  }
-`;
-
 export default function CharactersList() {
-  const { error, data, loading } = useQuery(GET_CHARACTERS);
-
-  //console.log({ error, loading, data });
+  const { error, loading, data } = useCharacters();
 
   if (loading) {
     return <div>Loading</div>;
