dd5b7c0c4eee0f2fdf95650b1a74885da8b20a03
mark <markpackham1@gmail.com>
2022-04-18 15:22:29 +0100

    output specific character
    https://youtu.be/gAbIQx26wSI?t=2657

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,10 +1,39 @@
 import React from "react";
+import "./Character.css";
 import { useCharacter } from "../hooks/useCharacter";
 
 export default function Character() {
   const { data, loading, error } = useCharacter(2);
 
-  console.log(error, loading, data);
+  if (loading) {
+    return <div>Loading</div>;
+  }
 
-  return <div className="Character">Character</div>;
+  if (error) {
+    return <div>Something went wrong</div>;
+  }
+
+  return (
+    <div className="Character">
+      <img
+        src={data.character.image}
+        alt={data.character.name}
+        width={750}
+        height={750}
+      />
+      <div className="Character-content">
+        <h1>{data.character.name}</h1>
+        <p>{data.character.gender}</p>
+        <div className="Character-episode">
+          {data.character.episode.map((episode) => {
+            return (
+              <div key={episode.id}>
+                {episode.name} - <b>{episode.episode}</b>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
