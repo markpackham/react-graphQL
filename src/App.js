4a162213b98744ff9abad53f97651df667b2c979
mark <markpackham1@gmail.com>
2022-04-18 14:29:46 +0100

    output CharactersList using version 6 of react-router-dom
    https://youtu.be/gAbIQx26wSI?t=2073

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import CharactersList from "./pages/CharactersList";
+import { Routes, Route } from "react-router";
 
 function App() {
   return (
-    <div>
-      <CharactersList />
+    <div className="App">
+      <Routes>
+        <Route strict exact path="/" element={<CharactersList />} />
+      </Routes>
     </div>
   );
 }
