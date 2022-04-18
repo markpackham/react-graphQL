a0cec2aec4b45e780e0ae59c843bf81c09b5c53c
mark <markpackham1@gmail.com>
2022-04-18 15:08:25 +0100

    create useCharacter hook
    https://youtu.be/gAbIQx26wSI?t=2329

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { useCharacter } from "../hooks/useCharacter";
 
 export default function Character() {
+  const { data, loading, error } = useCharacter();
+
   return <div>Character</div>;
 }
