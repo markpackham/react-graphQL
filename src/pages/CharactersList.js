6e4c864479265569417ec93845f39fa3453451d6
mark <markpackham1@gmail.com>
2022-04-18 15:46:12 +0100

    use Link in CharacterList
    https://youtu.be/gAbIQx26wSI?t=2715

--- src/pages/CharactersList.js
+++ src/pages/CharactersList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useCharacters } from "../hooks/useCharacters";
 import "./CharactersList.css";
 
@@ -17,10 +18,10 @@ export default function CharactersList() {
     <div className="CharactersList">
       {data.characters.results.map((character) => {
         return (
-          <div key={character.id}>
+          <Link to={`/${character.id}`} key={character.id}>
             <img src={character.image} alt={character.name} />
             <h2>{character.name}</h2>
-          </div>
+          </Link>
         );
       })}
     </div>
